b09dc0a986c47066a6b2d6b1ecf380637a90e50f
michaeltreml <miketreml@mac.com>
2021-01-14 15:34:45 -0600

    putin sample login info

--- Login Scrapper.py
+++ Login Scrapper.py
@@ -8,12 +8,12 @@ options.add_argument('--incognito')
 options.add_argument('--headless')
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-urlLogin = "https://www.amazon.com"
+urlLogin = "https://www.amazon.com/ap/signin?clientContext=131-0211287-5980619&openid.return_to=https%3A%2F%2Fwww.audible.com%2F&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=audible_shared_web_us&openid.mode=checkid_setup&marketPlaceId=AF2M0KC94RCEA&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&pageId=amzn_audible_bc_us&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0"
 
 
 driver.get(urlLogin)
 time.sleep(2)
-driver.find_element_by_xpath("""//*[@id="username"]""").send_keys('')
-driver.find_element_by_xpath("""//*[@id="password"]""").send_keys('')
+driver.find_element_by_xpath("""//*[@id="ap_email"]""").send_keys('sample@test.com')#login id
+driver.find_element_by_xpath("""//*[@id="ap_password"]""").send_keys('sample password')# password
 time.sleep(2)
-driver.find_element_by_xpath("""//*[@id="modalLogin"]/div/div/div[2]/form/button""").click()
+driver.find_element_by_xpath("""//*[@id="signInSubmit"]""").click()
